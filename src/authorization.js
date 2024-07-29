16b7f1620a290e7dd35cfb257f0ad2e420cde270
Fyodor Khruschov <fkhruschov@roketus.com>
2024-07-29 17:31:27 +0300

    remove client check

--- src/authorization.js
+++ src/authorization.js
@@ -9,19 +9,19 @@ export const authorization = {
   init: async() => {
     return new Promise((resolve, reject) => {
       const interval = setInterval(() => {
-        if (window.Telegram &&
-          window.Telegram.WebApp.initDataUnsafe &&
-          window.Telegram.WebApp.initDataUnsafe.user) {
+        // if (window.Telegram &&
+        //   window.Telegram.WebApp.initDataUnsafe &&
+        //   window.Telegram.WebApp.initDataUnsafe.user) {
           clearInterval(interval);
-          const initData = window.Telegram.WebApp.initData;
-          // const initData = 'query_id=AAGYEdk-AAAAAJgR2T7stF57&user=%7B%22id%22%3A1054413208%2C%22first_name%22%3A%22Fyodor%22%2C%22last_name%22%3A%22Khruschov%22%2C%22username%22%3A%22elevenmins%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1721316039&hash=69bf3ee5020e663b85db82e3d321956a28e334929781977249aa46d5eb4df934'
+          // const initData = window.Telegram.WebApp.initData;
+          const initData = 'query_id=AAGYEdk-AAAAAJgR2T7stF57&user=%7B%22id%22%3A1054413208%2C%22first_name%22%3A%22Fyodor%22%2C%22last_name%22%3A%22Khruschov%22%2C%22username%22%3A%22elevenmins%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1721316039&hash=69bf3ee5020e663b85db82e3d321956a28e334929781977249aa46d5eb4df934'
           //
           const result = authorization.isValidHash(initData);
-          if (result) {
+          // if (result) {
             httpClient.defaults.headers['tg_query'] = initData;
-          }
-          resolve({ result, tg_query: initData });
-        }
+          // }
+          resolve({ result: true, tg_query: initData });
+        // }
       }, INTERVAL);
     });
   },
@@ -34,7 +34,9 @@ export const authorization = {
     const data_check_string = items.join('\n')
 
     const secret_key = authorization._HMAC_SHA256(authorization.bot_token, 'WebAppData')
-    const hashGenerate = authorization._hex(authorization._HMAC_SHA256(data_check_string, secret_key))
+    const hashGenerate = authorization._HMAC_SHA256(data_check_string, secret_key)
+
+    console.log(hash, hashGenerate)
 
     return Boolean(hashGenerate === hash)
   },
@@ -42,9 +44,5 @@ export const authorization = {
   _HMAC_SHA256: (value, key) => {
     // return crypto.createHmac('sha256', key).update(value).digest()
     return CryptoJS.HmacSHA256(value, key).toString(CryptoJS.enc.Hex);
-  },
-
-  _hex: (bytes) => {
-    return bytes.toString('hex');
   }
 }
